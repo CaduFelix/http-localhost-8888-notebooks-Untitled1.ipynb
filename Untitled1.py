2bf4bcfc4e1f525664da465acb7090490aba0d0a
CaduFelix <50344506+CaduFelix@users.noreply.github.com>
2021-11-10 12:21:18 -0300

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(Boa noite)
+
+
+# In[2]:
+
+
+print ("boa noite")
+
+
+# In[3]:
+
+
+x = 5
+
+
+# In[4]:
+
+
+print ("x")
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+print (x)
+
+
+# In[6]:
+
+
+print (type(x))
+
+
+# In[7]:
+
+
+preco = 19.99
+print(preco, type(preco))
+
+
+# In[8]:
+
+
+cidade = "São Paulo"
+print (cidade, type(cidade))
+
+
+# In[9]:
+
+
+disponivel = True
+print (disponivel)
+
+
+# In[10]:
+
+
+disponivel = false 
+print (disponivel, type (disponivel))
+
+
+# In[11]:
+
+
+disponivel = False
+print (disponivel, type(disponivel))
+
+
+# In[12]:
+
+
+x = 50
+y = 2
+
+
+# In[13]:
+
+
+print (x + y)
+print  (x - y)
+print (x * y)
+print (x / y)
+
+
+# In[14]:
+
+
+print (x ** y)
+print ( x // y)
+print (x % y)
+
+
+# In[15]:
+
+
+print ( 3 // 4)
+
+
+# In[16]:
+
+
+print (3 // 4)
+
+
+# In[17]:
+
+
+tem cafe = True 
+tem pao = False
+
+
+# In[18]:
+
+
+tem_cafe = True
+tem_pao = False
+
+
+# In[19]:
+
+
+print (not tem_cafe)
+print (tem_cafe or tem_pao)
+print tem_cafe and tem_pao)
+
+
+# In[20]:
+
+
+print (not tem_cafe)
+print (tem_cafe or tem_pao)
+print (tem_cafe and tem_pao)
+
+
+# In[21]:
+
+
+dolar = 5.3
+real = 1
+
+
+# In[22]:
+
+
+print (dolar > real)
+print (dolar < real)
+print (dolar == real)
+print (dolar >= real)
+print (dolar <= real)
+print (dolar != real)
+
+
+# In[23]:
+
+
+idade = input (" Informe a sua idade:")
+ptiny (idade, type (idade))
+
+
+# In[24]:
+
+
+idade = input (" Informe a sua idade :")
+print ( idade, type (idade))
+
+
+# In[25]:
+
+
+idade = int(idade)
+print (idade, type (idade))
+
+
+# In[26]:
+
+
+print (float("123.25"))
+print(str(123.225))
+print (bool(''))
+print (bool("abc"))
+print (bool(0))
+print (bool-2)
+
+
+# In[27]:
+
+
+print (float("123.25"))
+print(str(123.225))
+print (bool(''))
+print (bool("abc"))
+print (bool(0))
+print (bool(-2)) 
+
+
+# In[ ]:
+
+
+idade = input ("Informe a sua idade ")
+print(idade, type(idade))
+
+    
+    
+
+
+# In[2]:
+
+
+idade = input ("Informe a sua idade ")
+print(idade, type(idade))
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+idade = input ("Informe a sua idade")
+print (idade, type(idade))
+
+
+# In[7]:
+
+
+salario_mensal = input ("Digite o valor do seu salario mensal")
+salario_mensal = float (salario_mensal)
+
+gasto_mensal = input("digite o valor do seu gasto mensal em media: ")
+gasto_mensal = float (gasto_mensal)
+
+salario_total = salario_mensal * 12
+gasto_total = gasto_mensal * 12
+
+montante_economizado = salario_total = gasto_total 
+print(" o montante que voce pode esconomizar ao fim do ano é de ", montante_economizado)
+                     
+                     
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
